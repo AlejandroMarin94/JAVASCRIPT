5167367dbd29507c4e73f8ee78dbbb5544dd788f
Alejandro.Marin <4asjmamt@gmail.com>
2025-11-12 20:47:36 +0100

    practica sql

--- MYSQL/practica culpa victor.sql
+++ MYSQL/practica culpa victor.sql
@@ -106,6 +106,146 @@ values
    
    select course_name from courses where duration = 4;
    
+   ## 6. Mostrar los estudiantes que han nacido en el 1998.
+   
+   select concat( first_name ," ", last_name) as estudiantes from students 
+   where year(date_of_birth) = 1998;
+   
+   ## 7. Seleccionar los alumnos cuyo nombre empieza por 'J'.
+   
+   select first_name from students where first_name like "J%";
+
+   ## 8. Mostrar los alumnos cuyo número de DNI contiene '12'.
+   
+   select first_name from students where dni like "%12%";
+   
+   ## 9. Calcula la duración promedio de los cursos.
+   
+   select  sum(duration) / count(duration) as duracion_media from courses;
+   
+   ## 10. Obtener la cantidad de cursos que ha realizado el alumno 'Jhon'.
+   
+	
+   
+   select first_name, count(e.course_id) as cursos_totales from students s 
+   inner join enrollments e on s.student_id = e.student_id
+   where s.first_name ="John"
+   group by s.first_name;
+   
+   ##11 11. Obtener el nombre y apellidos de los estudiantes matriculados en el curso de "Computer Science".
+   
+   select s.first_name, s.last_name from students s 
+   inner join enrollments e on e.student_id = s.student_id 
+   inner join courses c on e.course_id = c.course_id
+   where c.course_name = "Computer Science";
+   
+   ##12 . Obtener el precio medio de los cursos por horario.
+   
+   select schedule, sum(price)/ count(course_name) as precio_medio from courses 
+   group by schedule;
+   
+   ##13. Obtén el curso más antiguo.
+   
+   select c.course_name, min(e.year_enrolled) as curso_mas_antiguo from courses c
+   inner join enrollments e on c.course_id = e.course_id
+   group by c.course_id, c.course_name
+   order by curso_mas_antiguo asc
+   limit 1;
+   
+   ## 14. Obtener el nombre y apellidos de los estudiantes
+   ## que se matricularon en algún curso en el año 2021.
+   
+   select concat(s.first_name, " " , s.last_name ) as nombre_completo , min(e.year_enrolled) as primer_año from students s 
+   inner join enrollments e on s.student_id = e.student_id
+	where e.year_enrolled = 2021
+   group by s.student_id, nombre_completo;
+   
+   ##15. Obtener el nombre del curso y su precio que se hayan matriculado en 2022 y tengan horario de mañana.
+   
+   select c.course_name as nombre_curso, c.price as precio_curso, c.schedule as horario, e.year_enrolled as matriculas from courses c 
+   inner join enrollments e on c.course_id = e.course_id
+   where e.year_enrolled = 2022
+   and c.schedule = "Morning"
+   group by c.course_id, c.course_name;
+
+  
+ ## 16. Obtener el gasto de cada uno de los alumnos, es decir, la suma total del precio de los cursos en los que se ha matriculado .
+ 
+ select  concat(s.first_name , " ", s.last_name) as nombre_completo, sum(c.price) as precio_curso from students s
+ inner join enrollments e on s.student_id = e.student_id
+ inner join courses c on c.course_id = e.course_id
+ group by s.student_id, concat(s.first_name , " ", s.last_name);
+ 
+ ##17. Mostrar los cursos en orden ascendente según su precio.
+ 
+ select course_name from courses
+ order by price asc;
+ 
+ ##18 18. Crear una función para obtener el precio medio (sin decimales)  de todos los cursos. Tip: Para redondear sin decimales usar Round()
+ 
+ delimiter //
+ create function preciomedio(nombre_curso varchar(50))
+ returns int
+ deterministic
+ begin
+ declare precio_medio int;
+ select round(avg(price)) into precio_medio
+ from courses
+ where course_name=nombre_curso;
+ return precio_medio;
+ end //
+ delimiter ;
+ 
+ select preciomedio("Literature");
+ 
+ ## 19. Crear un procedimiento almacenado para mostrar el nombre y la cantidad 
+ ## de cursos de cada dificultad, ordenados por la cantidad de cursos de manera descendente.
+ 
+ 
+   delimiter //
+ create procedure mostrarCursosA()
+ begin
+ select c.course_name as nombre_curso, c.difficulty as nivel, count(c.difficulty) as cantidad from courses c
+ group by c.course_name, c.difficulty
+ order by count(c.difficulty) desc;
+ end //
+ delimiter ;
+ 
+ call mostrarCursosA();
+ 
+ ##20 20. Crear una vista llamada getAdvancedStudents para obtener el nombre y apellidos 
+ ## y la dificultad(nivel) de los estudiantes que se han matriculado en algún curso de nivel "Advanced" ordenada por nombre de forma ascendente.
+ 
+ 
+ create view getAdvancedStudentsAA as
+ select concat (s.first_name , " ", s.last_name) as nombre_completo, count(c.difficulty) from students s
+ inner join enrollments e on s.student_id = e.student_id
+ inner join courses c on e.course_id = c.course_id
+ 
+ where c.difficulty = "advanced"
+ group by s.student_id, nombre_completo
+ order by c.difficulty asc;
+ 
+ SELECT * FROM getAdvancedStudentsAA;
+ 
+
+
+ 
+
+ 
+ 
+
+   
+   
+ 
+   
+   
+   
+   
+
+
+   
+   
    
 
    
